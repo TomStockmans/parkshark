01645b58bf643002556468d4c4428f85a48cbef3
YanVkhv <yan.vkhv@gmail.com>
2019-11-18 12:00:03 +0100

    Fixed some major bugs

--- api/src/main/java/com/team4/api/member/MemberDto.java
+++ api/src/main/java/com/team4/api/member/MemberDto.java
@@ -7,63 +7,58 @@ import com.team4.domain.member.Name;
 import java.time.LocalDate;
 
 public class MemberDto {
-    private long id;
     private Name name;
     private Address address;
     private LicensePlate licensePlate;
     private String phoneNumber;
     private String email;
-    private LocalDate registrationDate;
 
-    public long getId() {
-        return id;
+    public MemberDto(Name name, Address address, LicensePlate licensePlate, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.licensePlate = licensePlate;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
     public Name getName() {
         return name;
     }
 
-    public Address getAddress() {
-        return address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LicensePlate getLicensePlate() {
-        return licensePlate;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public LocalDate getRegistrationDate() {
-        return registrationDate;
-    }
-
     public void setName(Name name) {
         this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
     }
 
     public void setAddress(Address address) {
         this.address = address;
     }
 
+    public LicensePlate getLicensePlate() {
+        return licensePlate;
+    }
+
     public void setLicensePlate(LicensePlate licensePlate) {
         this.licensePlate = licensePlate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setRegistrationDate(LocalDate registrationDate) {
-        this.registrationDate = registrationDate;
-    }
 }
