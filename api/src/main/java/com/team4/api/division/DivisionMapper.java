e27fee06ac968351a96754d77df29eabbd1f0539
Nick Lemaire <lemaire.nick@gmail.com>
2019-11-18 13:03:21 +0100

    Create subdivision

--- api/src/main/java/com/team4/api/division/DivisionMapper.java
+++ api/src/main/java/com/team4/api/division/DivisionMapper.java
@@ -6,7 +6,11 @@ import com.team4.domain.division.Division;
 public class DivisionMapper {
 
     public static Division mapToDivision(CreateDivisionDto divisionDto){
-        return new Division(divisionDto.name, divisionDto.originalName, new Director(divisionDto.director.firstName, divisionDto.director.lastName));
+        return new Division(divisionDto.name, divisionDto.originalName, new Director(divisionDto.director.firstName, divisionDto.director.lastName), null);
+    }
+
+    public static Division mapToDivision(CreateDivisionDto divisionDto, Division parent){
+        return new Division(divisionDto.name, divisionDto.originalName, new Director(divisionDto.director.firstName, divisionDto.director.lastName), parent);
     }
 
     public static DivisionDto mapToDivisionDto(Division division){
