381474b843accc2401808f891e8ef76a598f92e2
Nick Lemaire <lemaire.nick@gmail.com>
2019-11-19 10:14:29 +0100

    Start allocation: removed unused test files

--- service/src/test/java/com/team4/service/TestApplication.java
+++
@@ -1,7 +0,0 @@
-package com.team4.service;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.team4")
-public class TestApplication {
-}
