8f94e4c54da713359bc298ad59280f40583bd5ee
YanVkhv <yan.vkhv@gmail.com>
2019-11-19 14:03:22 +0100

    Added exception for getMemberByInvalidId

--- api/src/main/java/com/team4/api/member/MemberController.java
+++ api/src/main/java/com/team4/api/member/MemberController.java
@@ -5,7 +5,10 @@ import com.team4.api.member.dto.FindAllMembersDto;
 import com.team4.api.member.dto.MemberDto;
 import com.team4.api.member.mapper.MemberMapper;
 import com.team4.domain.member.Member;
+import com.team4.domain.member.MemberException;
 import com.team4.service.member.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,6 +20,8 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping(MemberController.RESOURCE_URL)
 public class MemberController {
+
+    private final Logger logger = LoggerFactory.getLogger(MemberController.class);
 
     public static final String RESOURCE_URL = "/members";
     private static final String APPLICATION_JSON_VALUE = MediaType.APPLICATION_JSON_VALUE;
@@ -38,8 +43,14 @@ public class MemberController {
     @GetMapping(path = "/{id}", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public MemberDto getMemberById(@PathVariable long id) {
-        Member member = memberService.getMemberById(id);
-        return MemberMapper.toDto(member);
+        try {
+            Member member = memberService.getMemberById(id);
+            return MemberMapper.toDto(member);
+        } catch (MemberException e) {
+            e.getMessage();
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
